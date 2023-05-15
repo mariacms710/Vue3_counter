fe8b3504cb7c4ead068eb7eac1bf057624b7e583
Maria Cecilia Morales <mariacemorales10@gmail.com>
2023-05-15 14:46:07 -0500

    Vue 3 app

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+  data: () => ({
+    contador:10,
+  }),
+  methods: {
+    incrementar() {
+      this.contador++;
+    },
+
+    disminuir() {
+      this.contador--;
+    },
+
+    reset() {
+      this.contador=10;
+    },
+
+  },
+
+};
+</script>
+
+<template>
+  <div class="container">
+    <img src="./assets/logo.svg" alt="Logo Vue" width="125" height="125" />
+    <small>
+      <h1>Hola mundo desde Vue.js 3</h1>
+    </small>
+    <hr />
+
+    <h2>Contador: {{contador}}</h2>
+    <button @click="incrementar()">+1</button>
+    <button @click="disminuir()">-1</button>
+    <button @click="reset()">Reset</button>
+  </div>
+</template>
+
+<style scoped>
+
+  .container{
+    text-align: center;
+    background-color: beige;
+  }
+
+  small{
+    color: blueviolet
+  }
+
+  h1{
+    font-weight: bolder;
+  }
+ 
+  h2{
+    font-size: 2.5rem;
+  }
+
+  button{
+    cursor: pointer;
+    font-size: 1.5rem;
+    margin: 0px 6px;
+
+  }
+</style>
